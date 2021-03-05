36c6ae10c0e13912dc9547395915d967def2eaa5
zegervdv <zegervdv@me.com>
2021-03-05 18:19:05 +0100

    Detect if word under cursor matches pattern and jump to start

--- init.lua
+++ init.lua
@@ -12,13 +12,20 @@ function M.increment(incr)
   local cursor = vim.api.nvim_win_get_cursor(0)
   local text = vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1]
 
-  -- TODO: when on the word, move to start of word
-  --       else jump to next word
-  local prefix, base, value = text:match(pattern, cursor[2])
+  -- Copied from nvim-cursorline.lua
+  local cursorword = vim.fn.matchstr(text:sub(1, cursor[2] + 1), [[\k*$]]) .. vim.fn.matchstr(text:sub(cursor[2] + 1), [[^\k*]]):sub(2)
+  local start = 0
+  if cursorword:match(pattern) then
+    _, start, _ = unpack(vim.fn.matchstrpos(text:sub(1, cursor[2] + 1), [[\k*$]]))
+  else
+    start = text:find(pattern, cursor[2]) - 1
+  end
+  print('Jumped to ' .. start)
+
+  local prefix, base, value = text:match(pattern, start)
   print(prefix .. ' + ' .. base .. ' + ' .. value)
   value = tonumber(value, bases[base]) + incr
   
-  local start = text:find(pattern, cursor[2]) - 1
 
   new_value = string.format("%s'%s%x", prefix, base, value)
   -- TODO: how to increase text range used?
